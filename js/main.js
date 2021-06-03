bbf8111e95d22d97c4aac57f0c276a12d19efe8c
Nataly <sweet9009@list.ru>
2021-06-03 18:37:23 +0300

    ДЗ2-Внесла функции в main.js - исправила ошибку2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 function getRandomFromRange(min, max) {
   if (min < 0 || min === 0 || min > max)  {
-    console.log("Неверно задан диапазон, минимальное значение должно быть больше нуля и больше максимального");
-    } else {
-      return Math.random() * (max - min) + min;
-      }
+    console.log('Неверно задан диапазон, минимальное значение должно быть больше нуля и больше максимального');
+  } else {
+    return Math.random() * (max - min) + min;
+  }
 }
 
-console.log(getRandomFromRange(1, 10));
+getRandomFromRange(1, 10);
 
 //Помощь искала https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random - взяла строчку "return Math.random() * (max - min) + min;"
 
 
-function getRandomFromRangeFloat(min, max, n) {
+function getRandomFromRangeFloat(min, max, round) {
   if (min < 0 || min > max)  {
-    console.log("Неверно задан диапазон, минимальное значение должно быть больше нуля и больше максимального");
-    } else {
-      let i = Math.pow(10,n);
-      return Math.round((Math.random() * (max - min) + min)*i)/i;
+    console.log('Неверно задан диапазон, минимальное значение должно быть больше нуля и больше максимального');
+  } else {
+    let fix = Math.pow(10,round);
+    return Math.round((Math.random() * (max - min) + min)*fix)/fix;
 
-      }
+  }
 }
 
-console.log(getRandomFromRangeFloat(0, 0.3, 2));
+getRandomFromRangeFloat(0, 0.3, 2);
 
 //Помощь искала https://javascript.ru/forum/misc/2860-okruglenie-do-nuzhnogo-kolichestva-znakov-posle-zapyatojj.html
