bdba1403121bdd4230894a723ea8aea7a50bfae5
Nataly <sweet9009@list.ru>
2021-06-03 18:24:05 +0300

    ДЗ2-Внесла функции в main.js - исправила ошибку

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ console.log(getRandomFromRange(1, 10));
 //Помощь искала https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random - взяла строчку "return Math.random() * (max - min) + min;"
 
 
-function getRandomFromRange(min, max, n) {
+function getRandomFromRangeFloat(min, max, n) {
   if (min < 0 || min > max)  {
     console.log("Неверно задан диапазон, минимальное значение должно быть больше нуля и больше максимального");
     } else {
@@ -21,6 +21,6 @@ function getRandomFromRange(min, max, n) {
       }
 }
 
-console.log(getRandomFromRange(0, 0.3, 2));
+console.log(getRandomFromRangeFloat(0, 0.3, 2));
 
 //Помощь искала https://javascript.ru/forum/misc/2860-okruglenie-do-nuzhnogo-kolichestva-znakov-posle-zapyatojj.html
