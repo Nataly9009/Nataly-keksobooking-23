b50f389b18e9554e7fea4ef0244f3a12d85c213e
Nataly <sweet9009@list.ru>
2021-06-10 17:04:40 +0300

    Домашнее задание Больше деталей1

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function getRandomFromRange(min, max) {
     return NaN;
   }
   return Math.round(Math.random() * (max - min) + min);
-};
+}
 
 //функция генерации случайного положительного числа с плавающей точкой
 
@@ -22,44 +22,43 @@ function getRandomFromRangeFloat(min, max, round) {
   }
   const fix = Math.pow(10,round);
   return Math.round((Math.random() * (max - min) + min)*fix)/fix;
-};
+}
 
 // Функция получения случайного елемента из массива
 
 function getRandomArrayElement(elements) {
   elements[getRandomFromRange(0, elements.length - 1)];
-};
+}
 
 //функция создания объявления
 
 function createAds () {
   const coordinates = {
     lat: getRandomFromRangeFloat(35.65000, 35.70000, 5),
-    lng: getRandomFromRangeFloat(139.70000, 139.80000, 5)
+    lng: getRandomFromRangeFloat(139.70000, 139.80000, 5),
   };
-
 return {
- author: {
-  avatar: 'img/avatar-${getRandomFromRange(1, 10)}.svg',
- },
+  author: {
+    avatar: 'img/avatar-${getRandomFromRange(1, 10)}.svg',
+      },
 
- offer: {
-  title: 'Заголовок объявления',
-  address: `${coordinates.lat}, ${coordinates.lng}`,
-  price: getRandomFromRange(1, 100000000),
-  type: getRandomArrayElement(TYPE),
-  room: getRandomFromRange(1,10),
-  guests: getRandomFromRange(1,100),
-  checkin: getRandomArrayElement(CHECKIN),
-  checkout:getRandomArrayElement(CHECKOUT),
-  features: getRandomArrayElement(FEATURES),
-  description: 'Описание помещения',
-  photos:getRandomArrayElement(PHOTOS)
-},
+  offer: {
+    title: 'Заголовок объявления',
+    address: `${coordinates.lat}, ${coordinates.lng}`,
+    price: getRandomFromRange(1, 100000000),
+    type: getRandomArrayElement(TYPE),
+    room: getRandomFromRange(1,10),
+    guests: getRandomFromRange(1,100),
+    checkin: getRandomArrayElement(CHECKIN),
+    checkout:getRandomArrayElement(CHECKOUT),
+    features: getRandomArrayElement(FEATURES),
+    description: 'Описание помещения',
+    photos:getRandomArrayElement(PHOTOS),
+    },
 
- location: coordinates,
-};
-};
+  location: coordinates,
+    };
+  }
 
 const similarAdsArray = new Array(adsNumber).fill(null).map(() => createAds());
 
